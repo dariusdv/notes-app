e5d222441ba152dc48b9aacb1bfeac40eed9c437
dariusdv <ddariusn@gmail.com>
2020-06-21 10:58:06 +0300

    Refactoring to use arrow functions

--- notes.js
+++ notes.js
@@ -2,15 +2,13 @@ const fs = require('fs');
 const chalk = require('chalk');
 const { argv } = require('process');
 
-const getNotes = function() {
+const getNotes = () => {
 	return 'Your notes...';
 };
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
 	const notes = loadNotes();
-	const duplicateNotes = notes.filter(function(note) {
-		return note.title === title;
-	});
+	const duplicateNotes = notes.filter((note) => note.title === title);
 
 	if (duplicateNotes.length === 0) {
 		notes.push({
@@ -25,11 +23,9 @@ const addNote = function(title, body) {
 	}
 };
 
-const removeNote = function(title) {
+const removeNote = (title) => {
 	const notes = loadNotes();
-	const noteRemoved = notes.filter(function(note) {
-		return note.title !== title;
-	});
+	const noteRemoved = notes.filter((note) => note.title !== title);
 
 	if (noteRemoved.length < notes.length) {
 		saveNotes(noteRemoved);
@@ -39,12 +35,12 @@ const removeNote = function(title) {
 	}
 };
 
-const saveNotes = function(notes) {
+const saveNotes = (notes) => {
 	const dataJSON = JSON.stringify(notes);
 	fs.writeFileSync('notes.json', dataJSON);
 };
 
-const loadNotes = function() {
+const loadNotes = () => {
 	try {
 		const dataBuffer = fs.readFileSync('notes.json');
 		const dataJSON = dataBuffer.toString();
